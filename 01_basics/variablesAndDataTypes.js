e8e9f42e559da67978a08f78591ddaf21f3afb4e
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2025-01-09 10:11:47 +0000

    2nd file modified

--- 01_basics/variablesAndDataTypes.js
+++ 01_basics/variablesAndDataTypes.js
@@ -1,2 +1,20 @@
 "use Strict"; // that now consider all our js code according to new code, not compulsory but considered good practice
- 
+ //alert(3+3) we are using node.js not browser and alert is not defined in node
+console.log("hi"); console.log("guys"); //code readability should be high this is not appreciated
+console.log(3
+    +
+    3
+);//considered bad practice
+//tc39.es(ecma script ) is the best documentation of js (https://tc39.es/ecma262/2023/multipage/)
+let name = "mradul" //string=> under suble quotes ""
+let age = 22 //number => 2 to power 53 and bigint for big numbers
+let isLoggedIn = false //boolean (true or false)
+// null => standlone value
+// undefined =>
+// symbol => major use in react used for uniqueness
+//object
+console.log(typeof null)
+console.log(typeof undefined)
+// data type of undefined is undefined but data type of null is object
+
+
