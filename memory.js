8ccdd9089130cf6c379ab2c78408a0b8f9af6a65
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2025-01-09 13:45:51 +0000

    memory

---
+++ memory.js
@@ -0,0 +1,19 @@
+// stack stores primitive data type values
+// queue uses non-primitive data type values
+ let myName = "harry"
+ yourName = myName
+ yourname = "hermoine"
+ console.log(myName); // here value gets changed in only latter variable
+  
+let Mradul = {
+    Nickname : "tashu",
+    age : 22,
+    hobby : 'dancing',
+    email : 'mradulsonare@gmail.com'
+}
+let Mranal = Mradul
+Mranal.Nickname = "tanu"
+console.log(Mradul);
+console.log(Mranal);    //here value gets changed in both the variables
+
+ 
