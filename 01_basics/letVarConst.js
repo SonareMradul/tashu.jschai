36d4aff8f433325145a8671f3c3875fea3be7029
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2025-01-09 09:25:06 +0000

    file name change and learnt let var and const

---
+++ 01_basics/letVarConst.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "mradulsonare@gmail.com"
+let accountPassword = "*******"
+// you can also define variable without using var or let keywordbut this practice is avoided
+accountCity = "Nagpur"
+let accountPincode = 461551
+// dont use var always use let because of block scope problem
+console.table([accountId,accountEmail,accountPassword,accountCity,accountPincode])
