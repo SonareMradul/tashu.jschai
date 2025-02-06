0c6c8409f91e5a46b4bea1d43642371fd8287353
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2025-02-06 11:24:28 +0000

    events learned

---
+++ 06_DOM/dom.js/domExplain.js
@@ -0,0 +1,5 @@
+// innertext---> ye us tag ka whi part dikhata h jo visible h
+// textContext --> ye us tag ka poora text dikhata h wo bhi jiski visibility none ho
+// innerHTML --> ye tag sahit poori html dikha deta h us tag ke andar ki excluding tag name 
+// us tag ke andar jo tag h uske sab attribute dikha dega
+// console me enter use krne ke liye shift+ENTER press krna pdta h
