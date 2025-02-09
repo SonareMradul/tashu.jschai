63e8ad204596d4e4657385490501d5cdc8c0f112
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2025-02-09 07:51:48 +0000

    oops startes

---
+++ 12_classesAndOOPS/classes.js
@@ -0,0 +1,54 @@
+const user = {
+    username : "hitesh",
+    loginCount : 3,
+    signedIn : true,
+    getUserDetails : function(){
+        console.log(`${this.username}`); // output - hitesh
+        console.log(this);
+        console.log(this); // output- user
+        
+        
+    }
+}
+console.log(user.getUserDetails());
+console.log(this); // output- {}
+console.log(this); // output- yahi line browser ke console me likhoge to window milega output me
+
+
+
+// ab pdhte h constructor('new' word) ki importance
+
+function mainUser(username,loginCount,signedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.signedIn = signedIn;
+    this.greet = function(){
+        console.log(`${this.username} is here`);
+        
+    }
+    // return this // it is implicit behaviour return nhi likhoge to bhi return krega
+}
+const user1 = mainUser('tashu',5, false)
+const user2 = mainUser('Mradul',10, true)
+console.log(user1); // isme user 2 ki values aayengi because user2 ne user1 ko overwrite kr liya isiliye hum constructor using new keyword use krenge because constructor hmesha ek new instance(copy) provide krta h object ki
+
+/* better approach is below */
+
+const user3 = new mainUser('manu',7, false)
+const user4 = new mainUser('raju',8, true)
+console.log(user3);
+console.log(user4.constructor); 
+//output - [function : mainUser]   mtlb ki constructor ek function h or uska naam bhi whi hota h jis function ka wo constructor hota h
+
+
+
+
+// oops ke baare me or pdhna h
+
+
+
+
+
+
+
+
--- 12_classesAndOOPS/notes.md
+++ 12_classesAndOOPS/notes.md
@@ -1,2 +1,24 @@
 ## DO JS REALLY HAS CLASSES--> yes it has classes. but just for sake of developers 
-coming from C++/Java background.
+## coming from C++/Java background.
+
+## Object
+-collection of properties and methods
+-ex. toLowerCase
+
+## why use OOP(you know already by first line)
+to make code more readable from sphagetti to clean.
+
+## Parts of OOP
+object literal
+
+## kya kya padhenge
+- constructor function
+- prototypes
+- classes
+- Instances(new , this)
+
+## four pillars of OOP
+- abstraction
+- encapsulation
+- inheritance(inheritance ka detail prototype wali (next) video me smjhenge)
+- polymorphism
